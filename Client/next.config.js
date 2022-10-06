755e421bcb48fc11b84de66b70f5c5bbb4ef1835
Ridho Purnomo <72849578+ridopurnomo86@users.noreply.github.com>
2022-10-06 10:29:27 +0700

    added node_env

--- Client/next.config.js
+++ Client/next.config.js
@@ -4,6 +4,7 @@ module.exports = {
         domains: ["source.unsplash.com", "res.cloudinary.com"],
     },
     env: {
+        NODE_ENV: process.env.NODE_ENV,
         COOKIE_USER: process.env.COOKIE_USER,
         BACKEND_URL: process.env.BACKEND_URL,
         JWT_TOKENKEY: process.env.JWT_TOKENKEY,
